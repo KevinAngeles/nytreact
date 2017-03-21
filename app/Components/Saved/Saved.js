7f3f42792c3bf49d5ae2f085b848695cf65ce496
KevinAngeles <kevin.angeles@outlook.com>
2017-03-21 16:58:37 -0500

    Deleted unnecessary comments below saved news

--- app/Components/Saved/Saved.js
+++ app/Components/Saved/Saved.js
@@ -38,9 +38,6 @@ class Saved extends React.Component {
             						<div className="col-md-4">Date Saved: {article.date}</div>
             						<div className="col-md-4"><button type="button" className="btn btn-danger" onClick={this.deleteSavedArticle} value={articleIndex}>Remove</button></div>
             					</div>
-            					<div className="row">
-            						<p className="col-md-12 text-center">Notes: What a wonderful achievement for mankind!</p>
-            					</div>
             				</div>
                         )
                     }
