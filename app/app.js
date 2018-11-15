581f1be0a2b449ca593b6c545710608d00fb1d63
KevinAngeles <kevin.angeles@outlook.com>
2018-11-15 14:32:00 -0600

    Webpack was updated to use polyfills and sass.

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 // Include the Main React Dependencies
-const React = require("react");
-const ReactDOM = require("react-dom");
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 // Include the Main Component
-import Main from "./Components/Main";
+import Main from './Components/Main'
+
+// Include Custom Style
+import './src/style/style.sass'
 
 // Renders the contents according to the route page.
-ReactDOM.render(<Main />, document.getElementById("app"));
+ReactDOM.render(<Main />, document.getElementById("app"))
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
-module.exports = {
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+module.exports = {
 	// This is the entry point or start of our react applicaton
-	entry: "./app/app.js",
+	entry: ['./app/app.js'],
 
 	// The plain compiled Javascript will be output into this file
 	output: {
-		path: __dirname + '/public',
+		path: path.resolve(__dirname, './public'),
 		filename: 'bundle.js',
 	},
 
@@ -17,29 +20,74 @@ module.exports = {
 				test: /\.jsx?$/,
 				// Webpack will only process files in our app folder. This avoids processing
 				// node modules and server files unnecessarily
-				include: /app/,
-				loader: "babel-loader",
-				options: {
-					// These are the specific transformations we'll be using.
-					presets: ["env", "react", "stage-2"]
-				}
-			},
-			{
-				// For all .css files except from node_modules
-				test: /\.css$/,
-				exclude: /node_modules/,
-				use: [
-					'style-loader',
-					{ loader: 'css-loader', options: { modules: true } }
-				]
-			},
-			{
+        include: /app/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                // These are the specific transformations we'll be using.
+                '@babel/preset-env',
+                {
+                  'targets'    : '> 0.25%, not dead',
+                  'useBuiltIns': 'entry'
+                }
+              ],
+              [
+                '@babel/preset-react'
+              ]
+            ]
+          }
+        }			  
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
 				// For all .css files in node_modules
 				test: /\.css$/,
 				include: /node_modules/,
 				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(sa|sc)ss$/,
+				use: [
+				  MiniCssExtractPlugin.loader,
+				  {
+            loader: 'css-loader', // translates CSS into CommonJS
+            options: {
+              url: false,
+              sourceMap: true
+            }
+				  },
+				  {
+            loader: 'sass-loader', // compiles Sass to CSS
+            options: {
+              sourceMap: true
+            }
+				  }
+				]
 			}
 		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+		  template: './app/src/index.html',
+		  filename: 'index.html'
+		}),
+		new MiniCssExtractPlugin({
+		  filename: '[name].css',
+		  chunkFilename: '[id].css'
+		})
+	],
+	stats: {
+		colors: true
 	},
 	// This lets us debug our react code in chrome dev tools. Errors will have lines and file names
 	// Without this the console says all errors are coming from just coming from bundle.js
